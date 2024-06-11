cf3d281f50fb42515470923e0cd841c596870063
Fetwar <fetwar@fetwar.com>
2024-06-11 13:35:32 +1000

    Enhance security by not loading token into env var

--- start.sh
+++ start.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 ORGANIZATION=$ORGANIZATION
-ACCESS_TOKEN=$(cat $ACCESS_TOKEN_FILE)
-
-REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
+REG_TOKEN=$(curl -sX POST -H "Authorization: token $(cat $ACCESS_TOKEN_FILE)" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
 
 cd /home/docker/actions-runner
 
